999a12075a829f0c6b7d1a78575280c15d3033ef
dargaCode <darga.code@gmail.com>
2020-12-08 16:07:34 -0800

    Remove hello world code

--- src/components/about/AboutSection.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import AboutSection from "./AboutSection";
-
-export default {
-  title: "About/AboutSection",
-  component: AboutSection
-};
-
-export const Basic = (): JSX.Element => <AboutSection />;
--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
-import NavigationBar from "./components/navigation/NavigationBar";
-import AboutSection from "./components/about/AboutSection";
-import SkillsSectionContainer from "./components/skills/SkillsSectionContainer";
 
 function App(): JSX.Element {
-  // required when hosting the app on a sub-directory of a domain rather than top-level
-  const projectPath =
-    process.env.NODE_ENV === "production" ? "/votes-by-city/" : "/";
-
   return (
-    <Router basename={projectPath}>
-      <div className="router-container">
-        <div id="nocontent">
-          {/* ignored by google */}
-          <h1 className="invisible-but-outline-readable">
-            Darga Webpack/React Template
-          </h1>
-        </div>
-        <header>
-          <NavigationBar />
-        </header>
-
-        <Switch>
-          <Route path="/hello" component={AboutSection} />
-          <Route path="/world" component={SkillsSectionContainer} />
-
-          <Redirect from="/" to="hello" />
-        </Switch>
-      </div>
-    </Router>
+    <div className="container">
+      <h1>Votes by City</h1>
+    </div>
   );
 }
 
